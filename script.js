ea36636277515e1ce9f17f2978b9c7074d538c01
FÊNNED SANTOS LIMA <118610718+sassukefenned@users.noreply.github.com>
2026-05-19 11:32:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ const books = [
     priceNum: 24.90,
     color: accentColors[2],
     coverImg: "imagens/Plano Alimentar – Desafio do Emagrecimento.png",
-    caktooLink: "https://pay.cakto.com.br/h5urmk2_891089", 
+    caktoLink: "https://pay.cakto.com.br/h5urmk2_891089",
     desc: "Cardápios, receitas e estratégias de jejum para perder peso de forma saudável. Um plano simples e funcional para resultados em apenas 7 dias.",
   },
   {
